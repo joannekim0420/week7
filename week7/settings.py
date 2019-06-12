3638a808f6973d1ac75d7fa11bcc325f532970dd
joannekim0420 <sall1412@naver.com>
2019-06-12 19:51:46 +0900

    heroku config

--- week7/settings.py
+++ week7/settings.py
@@ -138,4 +138,3 @@ ACCOUNT_LOGOUT_ON_GET = True
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-heroku config:set DISABLE_COLLECTSTATIC=1
